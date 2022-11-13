9f6e3441de1359ff6b4dcf51e6d3adae1270461e
Cameron McLaughlin <cronmclaughlin@gmail.com>
2022-11-13 06:11:51 -0500

    Add alwayscenter and movestack patches

--- config.h
+++ config.h
@@ -51,6 +51,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+#include "movestack.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -82,7 +83,9 @@ static const Key keys[] = {
 
 	{ MODKEY,			XK_b,	   spawn,	   SHCMD("firefox") },
 	{ MODKEY,			XK_f,	   spawn,	   SHCMD("pcmanfm") },
-	{ MODKEY,			XK_v,	   spawn,          SHCMD("discord") },
+	{ MODKEY,			XK_v,	   spawn,    SHCMD("discord") },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 
 	TAGKEYS(                        XK_1,                      0)
 
